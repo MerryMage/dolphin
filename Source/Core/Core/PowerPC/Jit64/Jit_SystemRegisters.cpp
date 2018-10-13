549979cde09335926264efdd4bd5b007815e25f8
MerryMage <MerryMage@users.noreply.github.com>
2018-10-13 14:53:00 +0100

    Jit_SystemRegisters: Emit SHR in mfspr SPR_TL/SPR_TU only if necessary

--- Source/Core/Core/PowerPC/Jit64/Jit_SystemRegisters.cpp
+++ Source/Core/Core/PowerPC/Jit64/Jit_SystemRegisters.cpp
@@ -337,7 +337,8 @@ void Jit64::mfspr(UGeckoInstruction inst)
           MOV(32, gpr.R(d), R(RAX));
         if (nextIndex == SPR_TL)
           MOV(32, gpr.R(n), R(RAX));
-        SHR(64, R(RAX), Imm8(32));
+        if (iIndex == SPR_TU || nextIndex == SPR_TU)
+          SHR(64, R(RAX), Imm8(32));
         if (iIndex == SPR_TU)
           MOV(32, gpr.R(d), R(RAX));
         if (nextIndex == SPR_TU)
